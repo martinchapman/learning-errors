e115fbddaec2c2ff3bd5aa5ba84096ee49b7d55c
Ofer Strichman <ofers@ie.technion.ac.il>
2013-03-06 15:12:25 +0000

    check for ERROR in output of cbmc

--- ce.bat
+++ ce.bat
@@ -1,6 +1,8 @@
 @echo off
 IF "%3" == "m" (echo #define membership > mode.c) else (echo #define conjecture > mode.c)
 cbmc -Iansi-c-lib --unwind %2 --no-unwinding-assertions --xml-ui %1 > tmp
+grep ERROR tmp
+IF %errorlevel% == 0 echo ERROR in output of CBMC
 IF NOT "%3" == "m" (grep -A 3 "<identifier>c::_Learn_ce_length</identifier>" tmp | grep "<value>" | tail -n 1 |  tr -d "<value>"/,\{\}  > model.txt ) 
 grep -A 3 "<identifier>c::_Learn_b</identifier>" tmp | grep "<value>" | tail -n 1 |  tr -d "<value>"/,\{\}  >> model.txt
 rem grep -q FAILURE tmp   // this is done inside online.cpp, because the return value does not always go through once it is in a batch file
