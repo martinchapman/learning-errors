29aeb0a88b8586d343df5d30a916ac004251dd98
Martin Chapman <martin.chapman@kcl.ac.uk>
2015-03-09 18:42:10 +0000

    Test Commit for merge

---
+++ demos/M1.c
@@ -0,0 +1,34 @@
+void a(int x) {
+    b(x);
+}
+
+void b(int x) {
+    c(x);
+}
+
+void c(int x) {
+    int y = nondet_int();
+    
+    if ( y < 10 ) {
+        f(x);
+    } else {
+        e(x);
+    }
+}
+
+void d(int x) {
+    e(x);
+}
+
+void e (int x) {
+    f(x);
+}
+
+void f(int x) {
+    _Learn_assert(x <= 10);
+}
+
+int main() {
+  d(x);  
+  e(x);  
+}
